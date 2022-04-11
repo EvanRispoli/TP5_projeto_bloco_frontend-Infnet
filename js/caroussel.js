de25d6614a24013c8dfbf5445db0a7525581fad9
Evan Rispoli <evan.rispoli@gmail.com>
2022-04-11 11:44:10 -0300

    TP5 Projeto de Bloco INFNET Prof Thiago

---
+++ js/caroussel.js
@@ -0,0 +1,63 @@
+$(document).ready(function()
+{
+    let caroussels = document.getElementsByClassName('image-caroussel');
+    let bigPoster = ['images/batman.png', 'images/top-gun-maverick.jpg', 'images/shazam.png'];
+    let time = 6000;
+    let bigPosterindex = 0;
+
+    [].forEach.call(caroussels, function(c)
+    {
+        let next = c.getElementsByClassName('next')[0],
+            prev = c.getElementsByClassName('prev')[0],
+            bubbles = c.getElementsByClassName('bubbles')[0],
+            inner = c.getElementsByClassName('inner')[0],
+            imgs = c.getElementsByTagName('img')[0],
+            currentImagenIndex = 0,
+            width = 244,
+            imgsArrayWidth = 4;
+
+            function switchImg()
+            {
+                inner.style.left = -width * currentImagenIndex + 'px';
+            }
+
+            next.addEventListener('click', function()
+            {
+                currentImagenIndex++;
+                if(currentImagenIndex >= imgsArrayWidth)
+                {
+                    currentImagenIndex = imgsArrayWidth;
+                }
+                switchImg();
+            });
+
+            prev.addEventListener('click', function()
+            {
+                currentImagenIndex--;
+                if(currentImagenIndex < 0)
+                {
+                    currentImagenIndex = 0;
+                }
+                switchImg();
+            });
+    });
+    
+    function automaticChange()
+    {
+        $('#bigPoster').attr('src', bigPoster[bigPosterindex]);
+        $('#backgroundFigure').css({'backgroundImage': "url(" + bigPoster[bigPosterindex] + ")", "backgroundSize":"cover"});
+
+        if(bigPosterindex < bigPoster.length - 1)
+        {
+            bigPosterindex++;
+        }
+        else
+        {
+            bigPosterindex = 0;
+        }
+        setTimeout(() => {
+            automaticChange(); 
+        }, time);
+    }
+    automaticChange();
+});
